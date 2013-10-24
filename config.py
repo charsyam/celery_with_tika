8223cb198b03782b342da6761b824ee913584f6e
charsyam <charsyam@gmail.com>
2013-10-24 09:08:51 +0900

    rollback to os.system

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config(object):
-    STATIC_FOLDER='/home/charsyam/text_extractor/static'
-    UPLOAD_FOLDER='/home/charsyam/text_extractor/uploads'
-    BUILD_FOLDER='/home/charsyam/text_extractor/builds'
+    STATIC_FOLDER='/home/charsyam/celery_with_tika/static'
+    UPLOAD_FOLDER='/home/charsyam/celery_with_tika/uploads'
+    BUILD_FOLDER='/home/charsyam/celery_with_tika/builds'
     BROKER='redis://localhost:6379/0'
     BACKEND='redis://localhost:6379/0'
     TIKA_CLASSPATH="/home/charsyam/tika-app.jar"
--- tasks.py
+++ tasks.py
@@ -2,28 +2,28 @@ import os
 from celery import Celery
 from config import Config
 
-os.environ['CLASSPATH'] = Config.TIKA_CLASSPATH
-from jnius import autoclass
-
 celery = Celery('tasks', broker=Config.BROKER, backend=Config.BACKEND)
 
-def extract_text(filename):
-    Tika = autoclass('org.apache.tika.Tika')
-    Metadata = autoclass('org.apache.tika.metadata.Metadata')
-    FileInputStream = autoclass('java.io.FileInputStream')
-
-    tika = Tika()
-    meta = Metadata()
-    contents = tika.parseToString(FileInputStream(filename), meta)
+def readfile(filename):
+    f = open(filename)
+    lines = f.readlines()
+    contents = ''.join(lines)
     return contents
 
 @celery.task
 def textextractor(workid):
     filename = "%s/%s"%(Config.UPLOAD_FOLDER, workid)
-    try:
-        contents = extract_text(filename)
-        return (contents, 0)
-    except:
+    builds = "%s/%s"%(Config.BUILD_FOLDER, workid)
+
+    command = "mkdir -p %s"%(builds)
+    ret = os.system(command)
+
+    os.chdir(builds)
+
+    command = "java -jar %s -t %s > output"%(Config.TIKA_CLASSPATH,filename)
+    ret = os.system(command)
+    if ret != 0:
         return ("tika error", -1)
-    finally:
-        os.remove(filename)
+
+    contents = readfile("output")
+    return (contents, 0)
