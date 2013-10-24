dd878b457e7921178a0f88598ac876ca1024de22
charsyam <charsyam@gmail.com>
2013-10-24 09:30:27 +0900

    change os.system to pipe

--- tasks.py
+++ tasks.py
@@ -1,29 +1,17 @@
 import os
+import shlex
+import subprocess
 from celery import Celery
 from config import Config
 
 celery = Celery('tasks', broker=Config.BROKER, backend=Config.BACKEND)
 
-def readfile(filename):
-    f = open(filename)
-    lines = f.readlines()
-    contents = ''.join(lines)
-    return contents
-
 @celery.task
 def textextractor(workid):
     filename = "%s/%s"%(Config.UPLOAD_FOLDER, workid)
-    builds = "%s/%s"%(Config.BUILD_FOLDER, workid)
+    command = "/usr/bin/java -jar %s -t %s > output"%(Config.TIKA_CLASSPATH,filename)
 
-    command = "mkdir -p %s"%(builds)
-    ret = os.system(command)
-
-    os.chdir(builds)
-
-    command = "java -jar %s -t %s > output"%(Config.TIKA_CLASSPATH,filename)
-    ret = os.system(command)
-    if ret != 0:
-        return ("tika error", -1)
-
-    contents = readfile("output")
+    args = shlex.split(command)
+    contents = subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
+    print contents
     return (contents, 0)
